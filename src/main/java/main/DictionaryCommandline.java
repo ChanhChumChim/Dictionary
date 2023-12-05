c2a9f91f9684575169888d6e63558c786adb59c3
ChanhChumChim <22028052@vnu.edu.vn>
2023-12-06 03:40:56 +0700

    asfasfsa

--- src/main/java/main/DictionaryCommandline.java
+++ src/main/java/main/DictionaryCommandline.java
@@ -1,6 +1,9 @@
 package main;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+import static main.DictionaryManagement.*;
 
 public class DictionaryCommandline extends Dictionary {
     public static String showAllWords() {
@@ -13,10 +16,85 @@ public class DictionaryCommandline extends Dictionary {
         return temp;
     }
 
+    public static void dictionaryAdvanced() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to My Application!");
+
+        while (true) {
+            System.out.println("[0] Exit");
+            System.out.println("[1] Add");
+            System.out.println("[2] Remove");
+            System.out.println("[3] Update");
+            System.out.println("[4] Display");
+            System.out.println("[8] Import from file");
+            System.out.println("[9] Export to file");
+
+            System.out.print("Your action: ");
+            int choice;
+
+            try {
+                choice = scanner.nextInt();
+            } catch (Exception e) {
+                // Xử lý ngoại lệ nếu người dùng nhập không phải là số
+                System.out.println("Action not supported");
+                scanner.nextLine(); // Đọc và loại bỏ ký tự không hợp lệ khỏi bộ đệm
+                continue;
+            }
+
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting the application. Goodbye!");
+                    return;
+                case 1:
+                    addWordFromUserInput();
+                    break;
+                case 2:
+                    removeWordFromUserInput();
+                    break;
+                case 3:
+                    updateWordFromUserInput();
+                    break;
+                case 4:
+                    showAllWords();
+                    break;
+                case 8:
+                    insertFromFile();
+                    break;
+                case 9:
+                    exportToFile();
+                    break;
+                default:
+                    System.out.println("Action not supported");
+            }
+        }
+    }
+
+    private static void addWordFromUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word: ");
+        String target = scanner.nextLine();
+        System.out.print("Enter the Vietnamese meaning: ");
+        String explain = scanner.nextLine();
+        addWord(target, explain);
+    }
+
+    private static void removeWordFromUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word to remove: ");
+        String target = scanner.nextLine();
+        removeWord(target);
+    }
+
+    private static void updateWordFromUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word to update: ");
+        String target = scanner.nextLine();
+        System.out.print("Enter the new Vietnamese meaning: ");
+        String explain = scanner.nextLine();
+        modifyWord(target, explain);
+    }
     public static void main(String[] args) throws IOException {
-        DictionaryManagement.insertFromFile();
-        DictionaryManagement.insertFromCommandLine();
-        System.out.println(showAllWords());
+        dictionaryAdvanced();
     }
 }
-
--- src/main/java/main/DictionaryManagement.java
+++ src/main/java/main/DictionaryManagement.java
@@ -5,9 +5,9 @@ import java.util.*;
 
 public class DictionaryManagement extends Dictionary {
     public static final String INPUT_PATH = "src/main/java/files/dictionaries.txt";
-    public static final String OUTPUT_PATH = "";
+    public static final String OUTPUT_PATH = "src/main/java/files/dictionaries_out.txt";
 
-    public static void insertFromCommandLine() {
+    public void insertFromCommandLine() {
         Scanner getStringInput = new Scanner(System.in);
         Scanner getIntegerInput = new Scanner(System.in);
         int count = getIntegerInput.nextInt();
@@ -38,5 +38,107 @@ public class DictionaryManagement extends Dictionary {
         }
     }
 
+    public static void exportToFile() {
+        try {
+            File file = new File(OUTPUT_PATH);
+            OutputStream outputStream = new FileOutputStream(file);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+            String format = "%-15s %-15s%n";
+            for (Word word : words) {
+                bufferedWriter.write(String.format(format, word.getWord_target(), word.getWord_explain()));
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void updateWordToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(INPUT_PATH);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (Word word : words) {
+                bufferedWriter.write(word.getWord_target() + "," + word.getWord_explain() + "\n");
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addWord(String searching, String meaning) {
+        searching = searching.toLowerCase();
+        meaning = meaning.toLowerCase();
+        int posAddWord = binaryCheck(0, words.size(), searching);
+        if (posAddWord == -1) {
+            System.out.println("Từ đã tồn tại!");
+            return;
+        }
+        words.add(new Word());
+        for (int i = words.size() - 2; i >= posAddWord; i--) {
+            words.get(i + 1).setWord_target(words.get(i).getWord_target());
+            words.get(i + 1).setWord_explain(words.get(i).getWord_explain());
+        }
+        words.get(posAddWord).setWord_target(searching);
+        words.get(posAddWord).setWord_explain(meaning);
+        updateWordToFile();
+    }
+
+    public static void removeWord(String searching) {
+        searching = searching.toLowerCase();
+        int index = Collections.binarySearch(words, new Word(searching, null));
+        if (index >= 0) {
+            words.remove(words.get(index));
+        } else {
+            System.out.println("Từ không tồn tại!");
+        }
+        updateWordToFile();
+    }
+
+    public static void modifyWord(String searching, String meaning) {
+        searching = searching.toLowerCase();
+        meaning = meaning.toLowerCase();
+        int pos = -1;
+        pos = Collections.binarySearch(words, new Word(searching, null));
+        if (pos >= 0) {
+            words.get(pos).setWord_explain(meaning);
+        } else {
+            System.out.println("Từ không tồn tại!");
+        }
+        updateWordToFile();
+    }
+
+    public static int binaryCheck(int start, int end, String word) {
+        if (end < start) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        int compareNext = word.compareTo(words.get(mid).getWord_target());
+        if (mid == 0) {
+            if (compareNext < 0) {
+                return 0;
+            } else if (compareNext > 0) {
+                return binaryCheck(mid + 1, end, word);
+            } else {
+                return -1;
+            }
+        } else {
+            int comparePrevious = word.compareTo(words.get(mid - 1).getWord_target());
+            if (comparePrevious > 0 && compareNext < 0) {
+                return mid;
+            } else if (comparePrevious < 0) {
+                return binaryCheck(start, mid - 1, word);
+            } else if (compareNext > 0) {
+                if (mid == words.size() - 1) {
+                    return words.size();
+                }
+                return binaryCheck(mid + 1, end, word);
+            } else {
+                return -1;
+            }
+        }
+    }
 }
